44703e46425fb66d773fca458b0ed806567a8466
sharkdp <davidpeter@web.de>
2014-11-15 11:04:04 +0100

    updated bootstrap script, use ppdflatex instead of pdflatex if it is installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+# Print message $2 with log-level $1 to STDERR, colorized if terminal
+log() {
+    local level=${1?}
+    shift
+    local code= line=">>> $*"
+    if [ -t 2 ]
+    then
+        case "$level" in
+            INFO) code=36 ;;
+            DEBUG) code=30 ;;
+            WARN) code=33 ;;
+            ERROR) code=31 ;;
+            *) code=37 ;;
+        esac
+        echo -e "\e[${code}m${line}\e[0m"
+    else
+        echo "$line"
+    fi >&2
+}
+
+
 if [[ $# != 1 ]]; then
-    echo "Usage: bootstrap.sh <name of main tex file>"
+    log ERROR "Usage: bootstrap.sh <name of main tex file>"
     exit 1
 fi
 
 if [[ ! -e latex-report ]]; then
-    echo "Call this script from the directory including the 'latex-report' repository"
+    log ERROR "Call this script from the directory including the 'latex-report' repository"
     exit 1
 fi
 
@@ -14,17 +35,30 @@ fi
 mainfile="${1}.tex"
 
 # Copy all bootstrap files
+log INFO "Creating '$mainfile' and standard settings"
 cp -iv latex-report/bootstrap/main.tex "$mainfile"
 cp -iv latex-report/bootstrap/.gitignore .
 cp -iv latex-report/bootstrap/.latexmkrc .
 
 # Create directories for figures and LaTeX output files
+log INFO "Creating 'fig' and 'dist' directory"
 mkdir -p fig
 mkdir -p dist
 
+# If pplatex is installed, use it:
+if ppdflatex -V > /dev/null; then
+    log INFO "ppdflatex is installed, using instead of pdflatex"
+    sed -i -e "s/'pdflatex/'ppdflatex/g" .latexmkrc
+else
+    log WARN "ppdflatex is not installed, using pdflatex"
+fi
+
 # Create git repository and make initial commit
 if [[ ! -e .git ]]; then
+    log INFO "Creating new git repository for this report"
     git init
     git add .
     git commit -m "bootstrapped from latex-report"
+else
+    log INFO "Git repository already exists"
 fi
--- bootstrap/.latexmkrc
+++ bootstrap/.latexmkrc
@@ -1,4 +1,9 @@
 $preview_continuous_mode = 1;
+
 $pdf_mode = 1;
+
 $out_dir = "dist";
+
 $pdflatex = 'pdflatex -interaction=nonstopmode -synctex=1 --shell-escape %O %S';
+
+# vim: syntax=perl
